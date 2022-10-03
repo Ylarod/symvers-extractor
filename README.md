0e551814c3018c2836a63e69eed7278ffdd3dcaf
Ylarod <me@ylarod.cn>
2022-10-03 18:07:10 +0800

    Init Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# symvers-extractor
+
+A tool to extractor symvers from a binary kernel
+
+## Install
+
+> pip3 install git+https://github.com/Ylarod/symvers-extractor
+
+## Credit
+
+[vmlinux-to-elf](https://github.com/marin-m/vmlinux-to-elf)
---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+from distutils.core import setup
+
+setup(name='symvers-extractor',
+      version='1.0',
+      description='A tool to extractor symvers from a binary kernel',
+      author='Ylarod',
+      author_email='',
+      url='https://github.com/Ylarod/symvers-extractor',
+      install_requires=['vmlinux-to-elf @ git+https://github.com/marin-m/vmlinux-to-elf'],
+      packages=[],
+      scripts=['symvers_extractor']
+     )
