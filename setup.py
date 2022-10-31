92e230c9603eb1d51e90d86dfd70fa85921bd09d
Ylarod <me@ylarod.cn>
2022-10-31 09:12:14 +0800

    add kallsyms_base

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup
 
 setup(name='symvers-extractor',
-      version='1.0',
-      description='A tool to extract symvers from a binary kernel',
+      version='1.1',
+      description='A tool to extractor symvers from a binary kernel',
       author='Ylarod',
       author_email='',
       url='https://github.com/Ylarod/symvers-extractor',
       install_requires=['vmlinux-to-elf @ git+https://github.com/marin-m/vmlinux-to-elf'],
       packages=[],
-      scripts=['symvers_extractor']
+      scripts=['symvers_extractor', 'kallsyms_base']
      )
